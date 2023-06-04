b22391995ca0a612f04e8f07bbd704837269c16d
Zeuswashere <tahaamir39@gmail.com>
2023-06-04 17:38:27 +0500

    Modifying last file to support float instead of int

--- Complex.h
+++ Complex.h
@@ -3,18 +3,18 @@
 #include <iostream>
 using namespace std;
 
-class IntComplex {
+class FloComplex {
 private:
 	int real;
 	int imaginary;
 public:
-	IntComplex(int real = 0, int imaginary = 0) : real(real), imaginary(imaginary) {}
+	FloComplex(int real = 0, int imaginary = 0) : real(real), imaginary(imaginary) {}
 	//getter functions
 	int getReal()const;
 	int getImaginary()const;
 	//operator overloading
-	IntComplex operator+(const IntComplex& add) const;
-	IntComplex operator-(const IntComplex& sub)const;
-	IntComplex operator*(const IntComplex& mul)const;
-	IntComplex operator/(const IntComplex& div)const;
+	FloComplex operator+(const FloComplex& add) const;
+	FloComplex operator-(const FloComplex& sub)const;
+	FloComplex operator*(const FloComplex& mul)const;
+	FloComplex operator/(const FloComplex& div)const;
 };
--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -3,31 +3,31 @@
 #include <iostream>
 #include "Complex.h"
 using namespace std;
-IntComplex::IntComplex(int real,int imaginary) : real(real),imaginary(imaginary){}
-IntComplex IntComplex::operator+(const IntComplex& add)const {
+FloComplex::FloComplex(int real,int imaginary) : real(real),imaginary(imaginary){}
+FloComplex FloComplex::operator+(const FloComplex& add)const {
 	int r1 = real + add.real;
 	int i1 = imaginary + add.imaginary;
-	return IntComplex(r1, i1);
+	return FloComplex(r1, i1);
 }
-IntComplex IntComplex::operator-(const IntComplex& sub)const {
+FloComplex FloComplex::operator-(const FloComplex& sub)const {
 	int r1 = real - sub.real;
 	int i1 = imaginary - sub.imaginary;
-	return IntComplex(r1, i1);
+	return FloComplex(r1, i1);
 }
-IntComplex IntComplex::operator*(const IntComplex& mul)const {
+FloComplex FloComplex::operator*(const FloComplex& mul)const {
 	int r1 = (real * mul.real) - (imaginary * mul.imaginary);//product of real minus product of imaginary
 	int	i1 = (real * mul.imaginary) + (imaginary * mul.real);// basically (r1*i2) + (i1*r1)
-	return IntComplex(r1, i1);
+	return FloComplex(r1, i1);
 }
-IntComplex IntComplex::operator/(const IntComplex& div)const {
+FloComplex FloComplex::operator/(const FloComplex& div)const {
 	int denominator = (div.real * div.real) + (div.imaginary * div.imaginary); //r2 square + i2 square 
 	int r1 = ((real * div.real) + (imaginary * div.imaginary)) / denominator;  //r1 square + i1 square /denominator
 	int i1 = ((imaginary * div.real) - (real * div.imaginary)) / denominator;  //((i1*r2)-(r1*i2)) /denominator
-	return IntComplex(r1, i1);
+	return FloComplex(r1, i1);
 }
-int IntComplex::getReal()const {
+int FloComplex::getReal()const {
 	return real;
 }
-int IntComplex::getImaginary()const {
+int FloComplex::getImaginary()const {
 	return imaginary;
 }
