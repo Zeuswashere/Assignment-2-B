61d7be53faad11b3d608f712e2f2ccee4979e511
Zeuswashere <tahaamir39@gmail.com>
2023-06-04 18:11:13 +0500

    Int Complex operation

--- Main.cpp
+++ Main.cpp
@@ -4,3 +4,10 @@
 #include "Complex.h"
 #include "Complex_f.h"
 using namespace std;
+int main() {
+	IntComplex a1 = (1,3);
+	IntComplex a2 = (2, 4);
+	IntComplex ans = a1 + a2;
+	cout << ans;
+	return 0;
+}
