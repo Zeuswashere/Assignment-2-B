d4332d9a263cc440161e14e2e4b0cbca7241a8cc
Zeuswashere <tahaamir39@gmail.com>
2023-06-04 16:44:11 +0500

    Multiplication of Complex int

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -12,4 +12,10 @@ IntComplex IntComplex::operator+(const IntComplex& add)const {
 IntComplex IntComplex::operator-(const IntComplex& sub)const {
 	int r1 = real - sub.real;
 	int i1 = imaginary - sub.imaginary;
+	return IntComplex(r1, i1);
 }
+IntComplex IntComplex::operator*(const IntComplex& mul)const {
+	int r1 = (real * mul.real) - (imaginary * mul.imaginary);//product of real minus product of imaginary
+		i1 = (real * mul.imaginary) + (imaginary * mul.real);// basically (r1*i2) + (i1*r1)
+		return IntComplex(r1, i1);
+}	
