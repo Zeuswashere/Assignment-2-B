40a1d0645a4d1962d472b0a1667a8d7b097ae7b0
Zeuswashere <tahaamir39@gmail.com>
2023-06-04 18:08:28 +0500

    initialisation

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 // Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
+#include "Complex.h"
 #include "Complex_f.h"
 using namespace std;
