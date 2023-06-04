ee5584a46bc736e4ae57b749876775952307ef49
Zeuswashere <tahaamir39@gmail.com>
2023-06-04 15:40:30 +0500

    Initialisation of Object and ADT and positive operator overloaded

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -1,5 +1,11 @@
 // Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
-#include <>
+#include "Complex.h"
 using namespace std;
+IntComplex::IntComplex(int real,int imaginary) : real(real),imaginary(imaginary){}
+IntComplex IntComplex::operator+(const IntComplex& add)const {
+	int r1 = real + add.real;
+	int i1 = imaginary + add.imaginary;
+	return IntComplex(r1, i1);
+}
